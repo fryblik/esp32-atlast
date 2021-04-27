99e0c01881dd4f9e8b05c1ac1cba7a9b0b494fcd
Vojtech Fryblik <adalbert.a10@gmail.com>
2021-04-27 21:13:11 +0200

    JavaScript code cleanup

--- data/www/esp32-atlast.js
+++ data/www/esp32-atlast.js
@@ -3,10 +3,13 @@
 import {CodeJar} from '/www/codejar.min.js';
 import {withLineNumbers} from '/www/linenumbers.min.js';
 
-let ws;				// WebSocket connection
-let pendingFile;	// File pending for upload
 
-// WebSocket connection and handling
+
+// WebSocket connection and handling //////////////////////////////////////////////////////////////
+
+let ws; // WebSocket connection
+
+// Connect to WebSocket
 function connectWS() {
 	ws = new WebSocket('ws://' + location.host + '/ws', ['arduino']);
 
@@ -51,7 +54,108 @@ function connectWS() {
 	}
 }
 
-// CLI: on ENTER key send input
+// Upload file
+function uploadFile() {
+	// Send file blob
+	ws.send(pendingFile);
+	console.log('File sent.');
+
+	// Clear pending file object
+	pendingFile = null;
+
+	// Request updated file list
+	requestFileList();
+}
+	
+// Add message to history
+function updateCliOut(addition) {
+	let cliOut = $( '#cliOut' );
+
+	// Split message into lines, filter out empty strings
+	let lines = addition.split('\n').filter(s => s);
+
+	// Show lines separated with <br>
+	$.each (lines, function(i, line) {
+		if (!cliOut.is(':empty')) {
+			cliOut.append('<br>');
+		}
+		cliOut.append(line);
+	});
+
+	// Scroll to bottom
+	cliOut.scrollTop(Number.MAX_SAFE_INTEGER);
+}
+
+// Update file select from received array
+function updateFileList(paths) {
+	// Clear old options
+	$( '#fileSelect' ).empty();
+
+	// Add option for each array member
+	$.each(paths, function(index, path) {   
+		$( '#fileSelect' )
+			.append($( '<option></option>' )
+					   .text(path));
+   });
+}
+
+// Request file list
+function requestFileList() {
+	// Send file list request JSON
+	ws.send('{"type":"fileList"}');
+}
+
+
+
+// Code editor ////////////////////////////////////////////////////////////////////////////////////
+
+let jar; // CodeJar object
+
+// Create code editor
+function initEditor() {
+	// Highlighter
+	const highlight = editor => {
+		CodeMirror.runMode(editor.textContent, "forth", editor);
+	}
+	// Non-jQuery selector for CodeJar compatibility
+	let editor = document.querySelector(".editor");
+	// Turn tabs into spaces
+	let opts = {tab: ' '.repeat(4)};
+
+	// Initialize
+	jar = new CodeJar(
+		editor,
+		withLineNumbers(highlight),
+		opts);
+
+	// TODO: actions on code update?
+	// debug only, remove:
+	jar.onUpdate(updatedCode => { console.log(updatedCode) });
+
+	// Update contents
+	let newCode = 'sample code';
+	jar.updateCode(newCode);
+}
+
+
+
+// DOM event handlers /////////////////////////////////////////////////////////////////////////////
+
+let pendingFile; // File pending for upload
+
+// Handler: On button click, kill running ATLAST program
+function killProgram() {
+	// Send KILL request JSON, include parameter from checkbox
+	$( '#killButton' ).click(function() {
+		if ($( '#restartTaskBox ').is(':checked')) {
+			ws.send('{"type":"kill","restartTask":1}');
+		} else {
+			ws.send('{"type":"kill","restartTask":0}');
+		}
+	});
+}
+
+// Handler: on ENTER key send CLI input
 function sendInput() {
 	const cliIn = $( '#cliIn' );
 	cliIn.keypress(function() {
@@ -78,64 +182,98 @@ function sendInput() {
 		}
 	});
 }
-	
-// Add message to history
-function updateCliOut(addition) {
-	let cliOut = $( '#cliOut' );
 
-	// Split message into lines, filter out empty strings
-	let lines = addition.split('\n').filter(s => s);
+// Handler: On click download and save file from ESP
+function downloadFile() {
+	// On download button click
+	$( '#fileDnButton' ).click(function() {
+		// Selected file option
+		const selection = $( '#fileSelect option:selected' );
 
-	// Show lines separated with <br>
-	$.each (lines, function(i, line) {
-		if (!cliOut.is(':empty')) {
-			cliOut.append('<br>');
+		// Ignore disabled option
+		if (selection.prop('disabled')) {
+			return;
 		}
-		cliOut.append(line);
-	});
 
-	// Scroll to bottom
-	cliOut.scrollTop(Number.MAX_SAFE_INTEGER);
-}
-
-// Handler: On button click, kill running ATLAST program
-function killProgram() {
-	// Send KILL request JSON, include parameter from checkbox
-	$( '#killButton' ).click(function() {
-		if ($( '#restartTaskBox ').is(':checked')) {
-			ws.send('{"type":"kill","restartTask":1}');
-		} else {
-			ws.send('{"type":"kill","restartTask":0}');
-		}
+		// Get file path
+		let path = selection.text();
+		// Remove leading slash from path to give FileSaver a valid filename
+		let filename = path.substring(1);
+		// Download (FileSaver.js)
+		saveAs(path, filename);
 	});
 }
 
-// Get valid file path from input field
-// Return path if valid, null otherwise
-function getFilePathIn() {
-	// Input field
-	let filePathIn = $( '#filePathIn' );
-	let path = filePathIn.val();
-
-	// Check for missing/invalid file path and notify user on violation
-	if (!path) {
-		alert('Please specify a save path.');
-		filePathIn.focus();
-		return null;
-	} else if (path.length < 2 || path.charAt(0) != '/' || path.charAt(path.length - 1) == '/') {
-		alert('Please specify a valid save path.');
-		filePathIn.focus();
-		return null;
-	} else if (path.length > 31) {
-		alert('File path can be at most 31 characters long. Current length: ' + path.length);
-		filePathIn.focus();
-		return null;
-	}
-	
-	// File path is valid
-	return path;
+// Handler: Edit selected file from device storage
+function editDeviceFile() {
+	$( '#fileEditButton' ).click(function() {
+		// Selected file option
+		const selection = $( '#fileSelect option:selected' );
+		// Ignore disabled option
+		if (selection.prop('disabled')) {
+			return;
+		}
+		// Get file path
+		let path = selection.text();
+		// Update file path field with loaded file path
+		$( '#filePathIn' ).val(path);
+		// Fetch data from file's URL and put it in the editor
+		$.get(path, function(data) {
+			jar.updateCode(data);
+		});
+	});
 }
 
+// Handler: On click delete file from ESP
+function deleteFile() {
+	// On delete button click
+	$( '#fileDelButton' ).click(function() {
+		// Selected file option
+		const selection = $( '#fileSelect option:selected' );
+
+		// Ignore disabled option
+		if (selection.prop('disabled')) {
+			return;
+		}
+
+		// Send delete request with file path in JSON
+		let obj = {
+			type: 'delete',
+			path: selection.text()
+		};
+		ws.send(JSON.stringify(obj));
+	});
+}
+
+// Handler: On click create file object from editor content and request upload
+function requestCodeUpload() {
+	// Ask to upload file on button click
+	$( '#editorUpButton' ).click(function() {
+		// Get valid save path from input field
+		let path = getFilePathIn();
+		// Check if we got the path
+		if (!path) {
+			return;
+		}
+
+		// Get editor content
+		let code = jar.toString();
+
+		// Create file object with editor content
+		pendingFile = new Blob(
+			[code],
+			{type: "text/plain;charset=utf-8"}
+		);
+
+		// Send upload request in JSON
+		let obj = {
+			type: 'upload',
+			name: path,
+			size: code.length
+		};
+		ws.send(JSON.stringify(obj));
+	});
+}
 
 // Handler: On click let user select local file, and request upload
 function requestFileUpload() {
@@ -170,109 +308,30 @@ function requestFileUpload() {
 	});
 }
 
-// Upload file
-function uploadFile() {
-	// Send file blob
-	ws.send(pendingFile);
-	console.log('File sent.');
+// Get valid file path from input field
+// Return path if valid, null otherwise
+function getFilePathIn() {
+	// Input field
+	let filePathIn = $( '#filePathIn' );
+	let path = filePathIn.val();
 
-	// Clear pending file object
-	pendingFile = null;
-
-	// Request updated file list
-	requestFileList();
-}
-
-// Handler: On click download and save file from ESP
-function downloadFile() {
-	// On download button click
-	$( '#fileDnButton' ).click(function() {
-		// Selected file option
-		const selection = $( '#fileSelect option:selected' );
-
-		// Ignore disabled option
-		if (selection.prop('disabled')) {
-			return;
-		}
-
-		// Get file path
-		let path = selection.text();
-		// Remove leading slash from path to give FileSaver a valid filename
-		let filename = path.substring(1);
-		// Download (FileSaver.js)
-		saveAs(path, filename);
-	});
-}
-
-// Handler: On click delete file from ESP
-function deleteFile() {
-	// On delete button click
-	$( '#fileDelButton' ).click(function() {
-		// Selected file option
-		const selection = $( '#fileSelect option:selected' );
-
-		// Ignore disabled option
-		if (selection.prop('disabled')) {
-			return;
-		}
-
-		// Send delete request with file path in JSON
-		let obj = {
-			type: 'delete',
-			path: selection.text()
-		};
-		ws.send(JSON.stringify(obj));
-	});
-}
-
-// Request file list
-function requestFileList() {
-	// Send file list request JSON
-	ws.send('{"type":"fileList"}');
-}
-
-// Update file select from received array
-function updateFileList(paths) {
-	// Clear old options
-	$( '#fileSelect' ).empty();
-
-	// Add option for each array member
-	$.each(paths, function(index, path) {   
-		$( '#fileSelect' )
-			.append($( '<option></option>' )
-					   .text(path));
-   });
-}
-
-
-/////////////////////////// Code editor ////////////////////////////
-
-let jar;			// CodeJar object
-
-// Create code editor
-function initEditor() {
-	// Highlighter
-	const highlight = editor => {
-		CodeMirror.runMode(editor.textContent, "forth", editor);
+	// Check for missing/invalid file path and notify user on violation
+	if (!path) {
+		alert('Please specify a save path.');
+		filePathIn.focus();
+		return null;
+	} else if (path.length < 2 || path.charAt(0) != '/' || path.charAt(path.length - 1) == '/') {
+		alert('Please specify a valid save path.');
+		filePathIn.focus();
+		return null;
+	} else if (path.length > 31) {
+		alert('File path can be at most 31 characters long. Current length: ' + path.length);
+		filePathIn.focus();
+		return null;
 	}
-	// Non-jQuery selector for CodeJar compatibility
-	let editor = document.querySelector(".editor");
-	// Turn tabs into spaces
-	let opts = {tab: ' '.repeat(4)};
-
-	// Initialize
-	jar = new CodeJar(
-		editor,
-		withLineNumbers(highlight),
-		opts);
-
-	// TODO: actions on code update?
-	// debug only, remove:
-	jar.onUpdate(updatedCode => { console.log(updatedCode) });
-
-	// Update contents
-	let newCode = 'sample code';
-	jar.updateCode(newCode);
+	
+	// File path is valid
+	return path;
 }
 
 // Handler: On click select and load local file into editor
@@ -304,65 +363,20 @@ function editLocalFile() {
 	});
 }
 
-// Handler: On click create file object from editor content and request upload
-function requestCodeUpload() {
-	// Ask to upload file on button click
-	$( '#editorUpButton' ).click(function() {
-		// Get valid save path from input field
-		let path = getFilePathIn();
-		// Check if we got the path
-		if (!path) {
-			return;
-		}
 
-		// Get editor content
-		let code = jar.toString();
 
-		// Create file object with editor content
-		pendingFile = new Blob(
-			[code],
-			{type: "text/plain;charset=utf-8"}
-		);
+// Run these functions when DOM is ready //////////////////////////////////////////////////////////
 
-		// Send upload request in JSON
-		let obj = {
-			type: 'upload',
-			name: path,
-			size: code.length
-		};
-		ws.send(JSON.stringify(obj));
-	});
-}
-
-// Handler: Edit selected file from device storage
-function editDeviceFile() {
-	$( '#fileEditButton' ).click(function() {
-		// Selected file option
-		const selection = $( '#fileSelect option:selected' );
-		// Ignore disabled option
-		if (selection.prop('disabled')) {
-			return;
-		}
-		// Get file path
-		let path = selection.text();
-		// Update file path field with loaded file path
-		$( '#filePathIn' ).val(path);
-		// Fetch data from file's URL and put it in the editor
-		$.get(path, function(data) {
-			jar.updateCode(data);
-		});
-	});
-}
-
-/////////////// Run these handlers when DOM is ready ///////////////
-
+// Create code editor
 $(initEditor);
+// Connect to WebSocket
 $(connectWS);
+// Set up DOM event handlers
 $(killProgram);
 $(sendInput);
-$(requestFileUpload);
 $(downloadFile);
-$(deleteFile);
-$(editLocalFile);
-$(requestCodeUpload);
 $(editDeviceFile);
+$(deleteFile);
+$(requestCodeUpload);
+$(requestFileUpload);
+$(editLocalFile);
